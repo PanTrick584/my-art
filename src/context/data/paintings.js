5b1f8207e952f7318024dc1a1d1bb73f79b0552c
Patryk <chodacki.pc@gmail.com>
2024-05-20 22:18:35 +0200

    prepare data structures for categories

---
+++ src/context/data/paintings.js
@@ -0,0 +1,24 @@
+export const paintings = [
+    {
+        title: {
+            default: "Abstract composition #1, M's.D.",
+            pl: "Kompozycja abstrakcyjna #2, M's.D."
+        },
+        date: "2018",
+        images: [
+            "painting-square-2-2018.jpg"
+        ],
+        type: "painting"
+    },
+    {
+        title: {
+            default: "Abstract skull, ASP",
+            pl: "Abstrakcja z czaszki, ASP"
+        },
+        date: "2018",
+        images: [
+            "painting-tall-1-2018.jpg"
+        ],
+        type: "painting"
+    },
+]
